9adcc32dad4a182673f391eacd1e85399ffd049a
D <Dickerson.Morgan@outlook.com>
2022-12-16 15:26:24 -0500

    started working on table variables and seed info

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS classlist_db;
+CREATE DATABASE classlist_db;
+
+USE classlist_db;
+
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL
+);
+CREATE TABLE role(
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL
+);
+CREATE TABLE employee(
+ id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO department (id, first_name)
+VALUES (001, "Morgan"),
+       (002, "Sarah"),
+       (003, "Karen"),
+       (004, "Megan");

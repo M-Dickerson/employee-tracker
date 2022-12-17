3c97e191646bf15b4f07f2addefbaf283eab927d
D <Dickerson.Morgan@outlook.com>
2022-12-16 22:17:04 -0500

    added comments to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,14 @@
+-- allows the database to exist, be used, and to be created
 DROP DATABASE IF EXISTS employee_roster;
 CREATE DATABASE employee_roster;
 
 USE employee_roster;
-
+-- creates the department table
 CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(30) NOT NULL
 );
-
+-- creates the role table
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
@@ -17,12 +18,12 @@ CREATE TABLE role (
   REFERENCES department(id)
   ON DELETE SET NULL
 );
-
+-- creates the manager table
 CREATE TABLE manager (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   manager_name VARCHAR(30)
 );
-
+-- creates the employee table
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,

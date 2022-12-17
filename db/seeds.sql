e6cb5db50ab374d2d2ffccf353d5e71371020d62
D <Dickerson.Morgan@outlook.com>
2022-12-16 22:14:46 -0500

    added comments to seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,25 +1,25 @@
-
+-- a list of the employees departments
 INSERT INTO department (name)
 VALUES ("Raven"),
        ("Libra"),
        ("Adjudicator"),
        ("King"),
        ("MC");
--- list of employees roles, their saleries, and their department IDs
+-- a list of the employees roles, their saleries, and their department IDs
 INSERT INTO role (title, salary, department_id)
 VALUES ("Private Detective", 100000, 1),
        ("Senior Attorney", 200000, 2),
        ("Psychiatrist", 300000, 3),
        ("Company Owner", 400000, 4),
        ("Attorney", 500000, 5);
--- list of employee managers
+-- a list of the employees managers
 INSERT INTO manager (manager_name)
 VALUES ("NSB"),
        ("Celestine"),
        ("Giann"),
        ("Austin"),
        ("Artem");
--- employee data
+-- a list of employees names, their role IDs, and their manager IDs
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Luke", "Pearce", 1, 1),
        ("Artem", "Wing", 2, 2),
